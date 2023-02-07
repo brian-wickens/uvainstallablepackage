14294f1c84ad1a43dcc56576f90504ac88412153
brian-wickens <97416931+brian-wickens@users.noreply.github.com>
2023-02-06 20:31:47 -0500

    added foobar()

---
+++ shared.py
@@ -0,0 +1,37 @@
+import re
+
+def afunction():
+    print("This is the installed function")
+
+def clean_string(str_word):
+    """
+    INPUTS:
+    str_word     string string contining several words to clean
+    RETURNS:
+    string       the string after removal of extra spaces, punctuation and lowercasing
+    """
+    str_word = re.sub(r'\W+', ' ', str_word)
+    assert isinstance(str_word, str), "String expected but recieved type {} instead".format(type(str_word))
+
+    return str_word.strip()
+
+import re
+def space_compress(stocomp):
+    assert isinstance(stocomp, str), "Expected str but got {} instead".format(type(stocomp))
+    comp = re.sub(r'\s+', ' ', stocomp)
+    return comp.strip()
+
+def new_func():
+    pass
+
+def foobar(input_int):
+    assert isinstance(input_int, int), "Expected integer but got {} instead".format(type(input_int))
+
+    if(input_int%5 == 0 and input_int%3 == 0):
+        return 'FooBar'
+    elif(input_int%3 == 0):
+        return 'Foo'
+    elif(input_int%3 == 0):
+        return 'Foo'
+    else:
+        return str(input_int)
